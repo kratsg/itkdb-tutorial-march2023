2f8bd7d02a53d9c94e5d519c40c2cec394382f3c
Giordon Stark <kratsg@gmail.com>
2023-03-08 20:34:51 +0100

    update

--- 01-app.py
+++ 01-app.py
@@ -3,6 +3,22 @@ from textual_filedrop import FileDrop
 
 
 class EOSUploadApp(App):
+    DEFAULT_CSS = """
+        Screen {
+            align: center middle;
+        }
+    """
+
+    BINDINGS = [("q", "exit", "Quit"), ("d", "toggle_dark", "Toggle dark mode")]
+
+    def action_toggle_dark(self) -> None:
+        """An action to toggle dark mode."""
+        self.dark = not self.dark
+
+    def action_exit(self) -> None:
+        """An action to exit."""
+        self.exit()
+
     def compose(self) -> ComposeResult:
         yield FileDrop(id="filedrop")
 
